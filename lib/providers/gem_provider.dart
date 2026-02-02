c100cec9aadce25c0dc35de2aa5f1fa1a99700fd
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-02 16:27:42 +0530

    Create all local veribale

--- lib/providers/gem_provider.dart
+++ lib/providers/gem_provider.dart
@@ -5,4 +5,18 @@ import '../models/gem.dart';
 import '../config/supabase_config.dart';
 
 
-class GemProvider with ChangeNotifier {}
+class GemProvider with ChangeNotifier {
+  final SupabaseClient _supabase = Supabase.instance.client;
+  
+  List<Gem> _gems = [];
+  List<Gem> _myGems = [];
+  List<String> _favoriteGemIds = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  List<Gem> get gems => _gems;
+  List<Gem> get myGems => _myGems;
+  List<Gem> get favoriteGems => _gems.where((g) => g.isFavorite).toList();
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+}
