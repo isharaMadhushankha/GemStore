3ea7aabe2156acc7183ec493bca80a8d7af7a75a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-02 16:00:48 +0530

    Table names

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -3,6 +3,9 @@ class SupabaseConfig {
   static const String supabaseUrl = 'YOUR_SUPABASE_URL';
   static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 
-  
+  static const String usersTable = 'users';
+  static const String gemsTable = 'gems';
+  static const String favoritesTable = 'favorites';
+
 
 }
