d39efc639db72a471aebb5fc046cb50d18b13d20
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-02 17:56:52 +0530

    config

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,7 +1,7 @@
 class SupabaseConfig {
 
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const String supabaseUrl = 'https://gchpjquidgxalpzikvcp.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjaHBqcXVpZGd4YWxwemlrdmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMjQ4NDMsImV4cCI6MjA4NTYwMDg0M30.Dp8qNUK5AplqNN2eKwTy88K4judnwoeFlgRku5xvGXU';
 
   static const String usersTable = 'users';
   static const String gemsTable = 'gems';
